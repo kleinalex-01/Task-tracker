be34ecee794e08b8a7a2301a07731e1810d754a6
kleinalex-01 <152911144+kleinalex-01@users.noreply.github.com>
2024-09-27 17:05:21 +0200

    Completed project
    Some changes ins style.css .
    Complete work in app.js .
    Main function is addRow(), which basically does everything.

--- app.js
+++ app.js
@@ -1,16 +1,59 @@
 const inputField = document.getElementById("task-input");
-const inputValue = inputField.value;
 const inputIcon = document.getElementById("submit-icon");
 const grid = document.getElementById("grid-container");
+const gridItem = document.getElementsByClassName("grid-item");
 
 const addRow = () => {
     const newRow = document.createElement("div");
+    const task = document.createElement("span");
+    const newIcon = document.createElement("i");
+    const newIcon2 = document.createElement("i");
     newRow.classList.add("grid-item");
-    newRow.innerText = inputField.value;
+    task.innerText = inputField.value;
+    newIcon.classList.add("fa-regular", "fa-square");
+    newIcon2.classList.add("fa-solid", "fa-trash");
+    newRow.appendChild(newIcon);
+    newRow.appendChild(newIcon2);
+    newRow.appendChild(task);
     grid.appendChild(newRow);
+
+    newIcon.addEventListener("click", function() {
+        if(task.style.textDecoration === "line-through") {
+            task.style.textDecoration = "none";
+            newIcon.classList.remove("fa-square-check");
+            newIcon.classList.add("fa-square");
+        } else {
+            task.style.textDecoration = "line-through";
+            newIcon.classList.remove("fa-square");
+            newIcon.classList.add("fa-square-check");
+        }
+    });
+
+    newIcon2.addEventListener("click", function() {
+        newRow.remove();
+    });
+
+    inputField.value = "";
 };
 
 inputField.addEventListener("keydown",function(event) {
-    if (event.key === "Enter" || inputValue.length < 3) {
-        addRow();
-    }})
+    if (event.key === "Enter") {
+        let inputValue = inputField.value;
+        if (inputValue.length >= 3) {
+            addRow();
+        } else {
+            alert("bru")
+        };
+    };
+});
+
+inputIcon.addEventListener("click",function() {
+    let inputValue = inputField.value;
+        if (inputValue.length >= 3) {
+            addRow();
+        } else {
+            alert("bru")
+        };
+});
+
+
